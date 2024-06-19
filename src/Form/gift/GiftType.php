17e76b7f015ac6418eeca848fb89d480ae32ccb5
Maarten <maarten.wils1234@gmail.com>
2024-06-19 10:29:32 +0200

    Afbeeldingen aan keuzemenu toegevoegd

--- src/Form/gift/GiftType.php
+++ src/Form/gift/GiftType.php
@@ -19,6 +19,15 @@ class GiftType extends AbstractType
                     'Pet' => 3,
                     'Vlag' => 4,
                 ],
+                'choice_attr' => function ($choice, $key, $value) {
+                    $images = [
+                        1 => '/images/rode_sjaal.jpg',
+                        2 => '/images/zwarte_sjaal.jpg',
+                        3 => '/images/pet.jpg',
+                        4 => '/images/vlag.jpg',
+                    ];
+                    return ['data-image' => $images[$value]];
+                },
                 'expanded' => true,
                 'multiple' => false,
             ]);
